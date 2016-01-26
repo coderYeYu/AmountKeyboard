9361527671a8df5561a02b3f1f3eee8362bd539d
wuzubo <wuzb2011@sina.com>
2016-01-26 22:15:50 +0800

    Initial Commit

---
+++ AmountKeyboard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AmountKeyboard
+//
+//  Created by yeyu on 16/1/26.
+//  Copyright © 2016年 yeyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AmountKeyboard/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AmountKeyboard
+//
+//  Created by yeyu on 16/1/26.
+//  Copyright © 2016年 yeyu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
