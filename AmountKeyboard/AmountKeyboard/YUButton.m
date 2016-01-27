2cfe5c86968f4a585c058c9c65fa0704c30cacda
wuzubo <wuzb2011@sina.com>
2016-01-27 11:16:15 +0800

    *

--- AmountKeyboard/AmountKeyboard/YUButton.m
+++ AmountKeyboard/AmountKeyboard/YUButton.m
@@ -9,45 +9,45 @@
 #import "YUButton.h"
 
 @interface YUButton ()
-{
-    UIView      *blackView;
-}
+
+@property (nonatomic, strong) UIView    *effectView;
 
 @end;
 
 @implementation YUButton
 
+- (UIView *)effectView
+{
+    if (!_effectView) {
+        _effectView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.frame.size.width, self.frame.size.height)];
+        _effectView.layer.cornerRadius = self.layer.cornerRadius;
+        _effectView.layer.masksToBounds = self.layer.masksToBounds;
+        _effectView.userInteractionEnabled = NO;
+        _effectView.backgroundColor = [UIColor blackColor];
+        [self addSubview:_effectView];
+    }
+    return _effectView;
+}
+
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
 {
     [super touchesBegan:touches withEvent:event];
     
-    if (!blackView) {
-        blackView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.frame.size.width, self.frame.size.height)];
-        blackView.layer.cornerRadius = self.layer.cornerRadius;
-        blackView.layer.masksToBounds = self.layer.masksToBounds;
-        blackView.userInteractionEnabled = NO;
-        blackView.backgroundColor = [UIColor blackColor];
-    }
-    blackView.alpha = 0.15;
-    [self addSubview:blackView];
+    self.effectView.alpha = 0.15;
 }
 
 - (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
 {
     [super touchesEnded:touches withEvent:event];
     
-    if (blackView) {
-        blackView.alpha = 0;
-    }
+    self.effectView.alpha = 0;
 }
 
 - (void)touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event
 {
     [super touchesCancelled:touches withEvent:event];
     
-    if (blackView) {
-        blackView.alpha = 0;
-    }
+    self.effectView.alpha = 0;
 }
 
 - (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
@@ -59,14 +59,10 @@
     float x = 60;
     if (!CGRectContainsPoint(CGRectMake(-x, -x, self.frame.size.width+2*x, self.frame.size.height+2*x), touchPoint))
     {
-        if (blackView) {
-            blackView.alpha = 0;
-        }
+        self.effectView.alpha = 0;
         
     }else {
-        if (blackView) {
-            blackView.alpha = 0.15;
-        }
+        self.effectView.alpha = 0.15;
     }
 }
 
